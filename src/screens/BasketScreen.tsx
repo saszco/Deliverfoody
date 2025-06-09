ef74bc3f3f4a39f8c8990b40a23e3d2a916bcaf9
okorolchuk3 <okorolchuk3@gmail.com>
2025-06-09 09:45:10 +0300

    Added new BasketIcon component

--- src/screens/BasketScreen.tsx
+++ src/screens/BasketScreen.tsx
@@ -1,7 +1,55 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useNavigate} from '../hooks/useNavigation';
+import {useAppSelector} from '../hooks/reduxHooks';
+import {selectBasketItems} from '../store/slices/basketSlice';
+import restaurantCards from '../data/restaurantCards';
+import IRestaurantCard from '../interfaces/IRestaurantCards';
+import IBasket from '../interfaces/IBasket';
+
+type groupedBasketItems = {
+  restaurantId: string;
+  items: Pick<IBasket, 'dishId' | 'quantity'>[];
+};
 
 const BasketScreen = () => {
+  const navigation = useNavigate();
+  const basketItems = useAppSelector(selectBasketItems);
+  let groupedItems: groupedBasketItems[] = [];
+
+  useEffect(() => {
+    if (basketItems.length === 0) {
+      console.error('No items in the basket');
+    }
+
+    for (const item of basketItems) {
+      const existingRestaurantIndex = groupedItems.findIndex(
+        groupedItem => groupedItem.restaurantId === item.restaurantId,
+      );
+
+      if (existingRestaurantIndex !== -1) {
+        groupedItems[existingRestaurantIndex].items.push({
+          dishId: item.dishId,
+          quantity: item.quantity,
+        });
+      }
+
+      if (existingRestaurantIndex === -1) {
+        groupedItems.push({
+          restaurantId: item.restaurantId,
+          items: [
+            {
+              dishId: item.dishId,
+              quantity: item.quantity,
+            },
+          ],
+        });
+      }
+    }
+  }, []);
+
+  console.log('Grouped items:', groupedItems);
+
   return (
     <View>
       <Text>BasketScreen</Text>
